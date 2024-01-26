9da2b2c480825c4bb9b0fce1163a10e0e00bc4d3
Felipe Zanetti <felipe.zanetti@codificar.com.br>
2024-01-26 14:48:28 -0300

    Test

---
+++ index.js
@@ -0,0 +1,5 @@
+import Phone from "./src/components/Phone";
+
+export default {
+  PhoneInput: Phone
+};
---
+++ src/components/Phone/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Phone: React.FC = () => {
+  return (
+    <Container>
+      <Text>Phone 🇧🇷 🇧🇷 🇧🇷 🇧🇷 🇧🇷</Text>
+    </Container>
+  );
+};
+
+export default Phone
