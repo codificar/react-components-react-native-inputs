aeec632193c2eb0c46092fe80e09a60450b2056b
Felipe Zanetti <felipe.zanetti@codificar.com.br>
2024-03-27 15:22:18 -0300

    fix. format code

--- src/inputs/Phone/index.tsx
+++ src/inputs/Phone/index.tsx
@@ -1,23 +1,29 @@
 // Modules
-import React, { forwardRef, useCallback, useImperativeHandle, useRef, useState } from 'react';
-import { TextInput as TextInputRef } from 'react-native';
+import React, {
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
+import { TextInput as TextInputRef } from "react-native";
 import { Icon } from "react-native-elements";
 
 // Utils
-import { dataPhones } from '../../utils/constants/data/phone';
-import { defaultProps } from '../../utils/constants/props/phone';
-import { getStyles } from '../../utils';
+import { dataPhones } from "../../utils/constants/data/phone";
+import { defaultProps } from "../../utils/constants/props/phone";
+import { getStyles } from "../../utils";
 
 // Locales
-import { getTranslate } from '../../locales';
+import { getTranslate } from "../../locales";
 
 // Types
-import { IPropsPhone } from '../../Types';
+import { IPropsPhone } from "../../Types";
 import {
   IPhoneRef,
   IPreparedMask,
-  allPhoneInfoInTheWorld
-} from '../../Types/Phone';
+  allPhoneInfoInTheWorld,
+} from "../../Types/Phone";
 
 // Styles
 import {
@@ -30,8 +36,8 @@ import {
   Row,
   TextFlag,
   PreTextInput,
-  TextFlagName
-} from './styles';
+  TextFlagName,
+} from "./styles";
 
 const Phone: React.ForwardRefRenderFunction<IPhoneRef, IPropsPhone> = (
   {
@@ -44,115 +50,143 @@ const Phone: React.ForwardRefRenderFunction<IPhoneRef, IPropsPhone> = (
     countries = [],
     ...rest
   },
-  ref,
+  ref
 ) => {
-  const refInput = useRef<TextInputRef>(null)
+  const refInput = useRef<TextInputRef>(null);
 
-  const [themes] = useState(getStyles(theme))
-  const [translate] = useState(getTranslate(language))
+  const [themes] = useState(getStyles(theme));
+  const [translate] = useState(getTranslate(language));
 
-  const [maxLength, setMaxLength] = useState<number>(1)
-  const [errorMessage, setError] = useState<string>('')
-  const [valuePhone, setValuePhone] = useState<string>('')
-  const [isFocused, setIsFocus] = useState<undefined | string>()
+  const [maxLength, setMaxLength] = useState<number>(1);
+  const [errorMessage, setError] = useState<string>("");
+  const [valuePhone, setValuePhone] = useState<string>("");
+  const [isFocused, setIsFocus] = useState<undefined | string>();
 
-  const [lengthValid, setLengthValid] = useState<number>(1)
-  const [country, setCountry] = useState<allPhoneInfoInTheWorld>({} as allPhoneInfoInTheWorld)
+  const [lengthValid, setLengthValid] = useState<number>(1);
+  const [country, setCountry] = useState<allPhoneInfoInTheWorld>(
+    {} as allPhoneInfoInTheWorld
+  );
 
   const renderDataSelected = useCallback(() => {
-    let countriesFiltered = dataPhones
+    let countriesFiltered = dataPhones;
     if (countries.length > 0) {
-      countriesFiltered = countriesFiltered.filter(country => countries.includes(country.countryCode))
+      countriesFiltered = countriesFiltered.filter((country) =>
+        countries.includes(country.countryCode)
+      );
     }
 
     return countriesFiltered.map((value: allPhoneInfoInTheWorld, index) => {
       return {
         ...value,
         key: index,
-        label: value.callingCode + " " + translate.phone.country[value.countryCode],
-        component: <TextFlagName>{`${value.flag} ${value.callingCode} ` + translate.phone.country[value.countryCode]}</TextFlagName>
-      }
-    })
+        label:
+          value.callingCode + " " + translate.phone.country[value.countryCode],
+        component: (
+          <TextFlagName>
+            {`${value.flag} ${value.callingCode} ` +
+              translate.phone.country[value.countryCode]}
+          </TextFlagName>
+        ),
+      };
+    });
   }, []);
 
-  const changeCountry = useCallback((value: allPhoneInfoInTheWorld ) => {
+  const changeCountry = useCallback((value: allPhoneInfoInTheWorld) => {
     const largestMask = value.phoneMasks.reduce((larger, current) => {
       return current.length > larger.length ? current : larger;
     }, "");
 
-    setMaxLength(largestMask.length)
-    setCountry(value)
+    setMaxLength(largestMask.length);
+    setCountry(value);
   }, []);
 
   const onChangeText = useCallback((INPUT: string, MASKS: string[]) => {
-    setError('')
-    const input = INPUT.replace(/\D/g, '');
+    setError("");
+    const input = INPUT.replace(/\D/g, "");
 
-    if (!MASKS.length) return setError(translate.phone.error.countryNotFound)
+    if (!MASKS.length) return setError(translate.phone.error.countryNotFound);
 
     const largestMask = MASKS.reduce((larger, current) => {
       return current.length > larger.length ? current : larger;
     }, "");
 
-    if (input.length > largestMask.replace(/[^#]/g, '').length) {
-      const labelError = label ? label.toLocaleLowerCase() : translate.phone.label
-      return setError(translate.phone.error.labelInvalid.replace('{{label}}', labelError))
+    if (input.length > largestMask.replace(/[^#]/g, "").length) {
+      const labelError = label
+        ? label.toLocaleLowerCase()
+        : translate.phone.label;
+      return setError(
+        translate.phone.error.labelInvalid.replace("{{label}}", labelError)
+      );
     }
 
-    const preparedMask: IPreparedMask[] = MASKS.map(value => ({
-      mask: value.replace(/\d/g, '#'),
-      numSubstitutions: value.replace(/[^#]/g, '').length + value.replace(/\D/g, '').length,
+    const preparedMask: IPreparedMask[] = MASKS.map((value) => ({
+      mask: value.replace(/\d/g, "#"),
+      numSubstitutions:
+        value.replace(/[^#]/g, "").length + value.replace(/\D/g, "").length,
     }));
 
-    const chosen = preparedMask.find(m => input.length <= m.numSubstitutions) as IPreparedMask;
-    const {mask, numSubstitutions} = chosen
-    setLengthValid(numSubstitutions)
+    const chosen = preparedMask.find(
+      (m) => input.length <= m.numSubstitutions
+    ) as IPreparedMask;
+    const { mask, numSubstitutions } = chosen;
+    setLengthValid(numSubstitutions);
 
-    if (!!mask){
-      let output = '';
+    if (!!mask) {
+      let output = "";
       let countInput = 0;
 
       for (let i = 0; i < mask.length; i++) {
         if (countInput >= input.length) break;
-        output += mask[i] === '#' ? input[countInput++] : mask[i];
+        output += mask[i] === "#" ? input[countInput++] : mask[i];
       }
 
       setValuePhone(output);
-      getValue && getValue(output)
+      getValue && getValue(output);
     } else {
-      setError(translate.phone.error.maskNotFound)
+      setError(translate.phone.error.maskNotFound);
     }
   }, []);
 
   useImperativeHandle(ref, () => ({
-    setValuePhone: (valuePhone = '', valueCountry = 'BR') => {
-      if (!valueCountry) return setError(translate.phone.error.countryNotFound)
+    setValuePhone: (valuePhone = "", valueCountry = "BR") => {
+      if (!valueCountry) return setError(translate.phone.error.countryNotFound);
 
-      const currentPhone = dataPhones.find(country => valueCountry === country.countryCode)
+      const currentPhone = dataPhones.find(
+        (country) => valueCountry === country.countryCode
+      );
       if (currentPhone) {
-        changeCountry(currentPhone)
-        onChangeText(valuePhone.replace(currentPhone.callingCode, ""), currentPhone.phoneMasks)
+        changeCountry(currentPhone);
+        onChangeText(
+          valuePhone.replace(currentPhone.callingCode, ""),
+          currentPhone.phoneMasks
+        );
       }
     },
-    setValueCountry: (valueCountry = 'BR') => {
-      if (!valueCountry) return setError(translate.phone.error.countryNotFound)
+    setValueCountry: (valueCountry = "BR") => {
+      if (!valueCountry) return setError(translate.phone.error.countryNotFound);
 
-      const currentPhone = dataPhones.find(country => valueCountry === country.countryCode)
+      const currentPhone = dataPhones.find(
+        (country) => valueCountry === country.countryCode
+      );
       if (currentPhone) {
-        changeCountry(currentPhone)
+        changeCountry(currentPhone);
       }
     },
     getValuePhone: () => {
-      return { valuePhone, countryCode: country.countryCode, callingCode: country.callingCode }
+      return {
+        valuePhone,
+        countryCode: country.countryCode,
+        callingCode: country.callingCode,
+      };
     },
     setErrorPhone: (error: string) => setError(error),
     isValid: () => {
-      const onlyNumber = valuePhone.replace(/[^0-9]+/g, "")
-      return onlyNumber.length >= lengthValid
+      const onlyNumber = valuePhone.replace(/[^0-9]+/g, "");
+      return onlyNumber.length >= lengthValid;
     },
     focus: () => {
-      setIsFocus(theme?.colors?.primary)
-      refInput?.current?.focus()
+      setIsFocus(theme?.colors?.primary);
+      refInput?.current?.focus();
     },
   }));
 
@@ -160,8 +194,14 @@ const Phone: React.ForwardRefRenderFunction<IPhoneRef, IPropsPhone> = (
     <Container>
       <TextLabel
         focus={isFocused}
-        theme={!!errorMessage.length ? {...themes.font?.label, color: themes.font?.error.color} : themes.font?.label}
-      >{label || translate.phone.label}</TextLabel>
+        theme={
+          !!errorMessage.length
+            ? { ...themes.font?.label, color: themes.font?.error.color }
+            : themes.font?.label
+        }
+      >
+        {label || translate.phone.label}
+      </TextLabel>
       <Row>
         <Selected
           focus={isFocused}
@@ -170,16 +210,21 @@ const Phone: React.ForwardRefRenderFunction<IPhoneRef, IPropsPhone> = (
           renderItem={() => <></>}
           outline={!!theme?.outline}
           data={renderDataSelected()}
-          disable={disabled ? themes?.colors?.disabled : ''}
+          disable={disabled ? themes?.colors?.disabled : ""}
           onChange={(value) => {
-            changeCountry(value as allPhoneInfoInTheWorld)
-            setValuePhone('')
-            getValue && getValue('')
+            changeCountry(value as allPhoneInfoInTheWorld);
+            setValuePhone("");
+            getValue && getValue("");
           }}
         >
           <Row>
             <TextFlag>{country.flag}</TextFlag>
-            <Icon type="font-awesome" name="caret-down" size={25} color="#999" />
+            <Icon
+              type="font-awesome"
+              name="caret-down"
+              size={25}
+              color="#999"
+            />
           </Row>
         </Selected>
 
@@ -187,26 +232,32 @@ const Phone: React.ForwardRefRenderFunction<IPhoneRef, IPropsPhone> = (
           focus={isFocused}
           error={!!errorMessage.length ? themes?.font?.error?.color : ""}
           outline={!!theme?.outline}
-          disable={disabled ? themes?.colors?.disabled : ''}
+          disable={disabled ? themes?.colors?.disabled : ""}
         >
-          {country.callingCode && <PreTextInput editable={false} theme={themes?.font?.input}>{country.callingCode}</PreTextInput>}
+          {country.callingCode && (
+            <PreTextInput editable={false} theme={themes?.font?.input}>
+              {country.callingCode}
+            </PreTextInput>
+          )}
           <TextInput
             {...rest}
             ref={refInput}
             value={valuePhone}
             editable={!disabled}
             maxLength={maxLength}
-            onBlur={() => setIsFocus('')}
+            onBlur={() => setIsFocus("")}
             theme={themes?.font?.input}
             onFocus={() => setIsFocus(themes.colors.primary)}
             placeholder={placeholder || translate.phone.placeholder}
-            onChangeText={text => onChangeText(text, country.phoneMasks || [])}
+            onChangeText={(text) =>
+              onChangeText(text, country.phoneMasks || [])
+            }
           />
         </ContainerInput>
       </Row>
-      <TextError theme={themes?.font?.error} >{errorMessage}</TextError>
+      <TextError theme={themes?.font?.error}>{errorMessage}</TextError>
     </Container>
   );
 };
 
-export default forwardRef(Phone)
+export default forwardRef(Phone);
--- src/inputs/Phone/styles.ts
+++ src/inputs/Phone/styles.ts
@@ -1,6 +1,6 @@
 // Modules
-import styled, { css } from 'styled-components/native';
-import ModalSelector from 'react-native-modal-selector-searchable';
+import styled, { css } from "styled-components/native";
+import ModalSelector from "react-native-modal-selector-searchable";
 
 // Types
 import {
@@ -9,30 +9,34 @@ import {
   IStyleSelected,
   IStyleTextError,
   IStyleTextInput,
-  IStyleTextLabel
-} from '../../Types/Phone';
+  IStyleTextLabel,
+} from "../../Types/Phone";
 
 export const Container = styled.View`
-  width: 100%;
   display: flex;
+  flex: 1;
+  width: 100%;
   padding: 8px 0;
 `;
 
 export const TextLabel = styled.Text<IStyleTextLabel>`
   color: #000;
-  margin: 4px ;
+  margin: 4px;
 
-  ${props => (props.theme.color) &&
+  ${(props) =>
+    props.theme.color &&
     css`
       color: ${props.theme.color};
     `}
 
-  ${props => (props.focus) &&
+  ${(props) =>
+    props.focus &&
     css`
       color: ${props.focus};
     `}
 
-  ${props => (props.theme.size) &&
+  ${(props) =>
+    props.theme.size &&
     css`
       font-size: ${props.theme.size}px;
     `}
@@ -42,12 +46,14 @@ export const TextError = styled.Text<IStyleTextError>`
   color: #c80000;
   margin: 2px 8px;
 
-  ${props => (props.theme.color) &&
+  ${(props) =>
+    props.theme.color &&
     css`
       color: ${props.theme.color};
     `}
 
-  ${props => (props.theme.size) &&
+  ${(props) =>
+    props.theme.size &&
     css`
       font-size: ${props.theme.size}px;
     `}
@@ -59,33 +65,39 @@ export const ContainerInput = styled.View<IStyleContainerInput>`
   align-items: center;
   border-color: #ccc;
   border-bottom-width: 1px;
-  margin-left: 8px ;
-  padding: 0 8px ;
+  margin-left: 8px;
+  padding: 0 8px;
   height: 40px;
 
-  ${props => (props.disable) &&
+  ${(props) =>
+    props.disable &&
     css`
       background-color: ${props.disable};
     `}
 
-  ${props => (props.outline) &&
+  ${(props) =>
+    props.outline &&
     css`
       border: 1px solid #ccc;
       border-radius: 4px;
     `}
 
-  ${props => (props.focus) &&
+  ${(props) =>
+    props.focus &&
     css`
       border-color: ${props.focus};
     `}
 
-  ${props => (props.error) &&
+  ${(props) =>
+    props.error &&
     css`
       border-color: ${props.error};
       border-bottom-width: 1px;
     `}
 
-  ${props => (!!props.error && props.outline) &&
+  ${(props) =>
+    !!props.error &&
+    props.outline &&
     css`
       border-color: ${props.error};
       border-bottom-width: 1px;
@@ -97,12 +109,14 @@ export const ContainerInput = styled.View<IStyleContainerInput>`
 export const PreTextInput = styled.TextInput<IStylePreTextInput>`
   color: black;
 
-  ${props => (props.theme.color) &&
+  ${(props) =>
+    props.theme.color &&
     css`
       color: ${props.theme.color};
     `}
 
-  ${props => (props.theme.size) &&
+  ${(props) =>
+    props.theme.size &&
     css`
       font-size: ${props.theme.size}px;
     `}
@@ -111,18 +125,20 @@ export const PreTextInput = styled.TextInput<IStylePreTextInput>`
 export const TextInput = styled.TextInput<IStyleTextInput>`
   flex: 1;
 
-  ${props => (props.theme.color) &&
+  ${(props) =>
+    props.theme.color &&
     css`
       color: ${props.theme.color};
     `}
 
-  ${props => (props.theme.size) &&
+  ${(props) =>
+    props.theme.size &&
     css`
       font-size: ${props.theme.size}px;
     `}
 `;
 
-export const Selected = styled(ModalSelector)<IStyleSelected>`
+export const Selected = styled(ModalSelector) <IStyleSelected>`
   padding: 0 8px;
   border-color: #ccc;
   border-bottom-width: 1px;
@@ -130,48 +146,54 @@ export const Selected = styled(ModalSelector)<IStyleSelected>`
   align-items: center;
   justify-content: center;
 
-  ${props => (props.focus) &&
+  ${(props) =>
+    props.focus &&
     css`
       border-color: ${props.focus};
     `}
 
-  ${props => (props.disable) &&
+  ${(props) =>
+    props.disable &&
     css`
       background-color: ${props.disable};
     `}
 
-  ${props => (props.outline) &&
+  ${(props) =>
+    props.outline &&
     css`
       border: 1px solid #ccc;
       border-radius: 4px;
     `}
 
-    ${props => (props.error) &&
+    ${(props) =>
+    props.error &&
     css`
       border-color: ${props.error};
       border-bottom-width: 1px;
     `}
 
-  ${props => (!!props.error && props.outline) &&
+  ${(props) =>
+    !!props.error &&
+    props.outline &&
     css`
       border-color: ${props.error};
       border-bottom-width: 1px;
       border: 1px solid ${props.error};
       border-radius: 4px;
     `}
-`
+`;
 
 export const Row = styled.View`
   flex-direction: row;
   align-items: center;
-`
+`;
 
 export const TextFlag = styled.Text`
   font-size: 24px;
   margin-right: 8px;
-`
+`;
 
 export const TextFlagName = styled.Text`
   font-size: 16px;
   margin-right: 8px;
-`
+`;
