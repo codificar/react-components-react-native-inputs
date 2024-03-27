af86a25423999f6b933ecd385973fa3d9045fae9
Felipe Zanetti <felipe.zanetti@codificar.com.br>
2024-03-27 15:23:13 -0300

    fix. RNDatePicker

--- src/inputs/DatePicker/index.tsx
+++ src/inputs/DatePicker/index.tsx
@@ -1,25 +1,46 @@
 // Modules
-import React, { forwardRef, useCallback, useImperativeHandle, useState } from 'react';
-import { ptBR, es } from 'date-fns/locale';
+import React, {
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+  useState,
+} from "react";
+import { ptBR, es } from "date-fns/locale";
 import { Icon } from "react-native-elements";
-import { format, isValid as isValidDate, subYears, isBefore, isEqual } from 'date-fns';
-import RNDatePicker from 'react-native-date-picker'
+import {
+  format,
+  isValid as isValidDate,
+  subYears,
+  isBefore,
+  isEqual,
+} from "date-fns";
+import RNDatePicker from "react-native-date-picker";
 
 // Locales
-import { getTranslate } from '../../locales';
+import { getTranslate } from "../../locales";
 
 // Types
-import { IPropsDatePicker } from '../../Types';
-import { IDatePickerRef } from '../../Types/DatePicker';
+import { IPropsDatePicker } from "../../Types";
+import { IDatePickerRef } from "../../Types/DatePicker";
 
 // Utils
-import { getStyles } from '../../utils';
-import { defaultProps } from '../../utils/constants/props/datePicker';
+import { getStyles } from "../../utils";
+import { defaultProps } from "../../utils/constants/props/datePicker";
 
 // Styles
-import { Container, TextLabel, ContainerInput, TextInput, TextError, TextPlaceHolder } from './styles';
+import {
+  Container,
+  TextLabel,
+  ContainerInput,
+  TextInput,
+  TextError,
+  TextPlaceHolder,
+} from "./styles";
 
-const DatePicker: React.ForwardRefRenderFunction<IDatePickerRef, IPropsDatePicker> = (
+const DatePicker: React.ForwardRefRenderFunction<
+  IDatePickerRef,
+  IPropsDatePicker
+> = (
   {
     title,
     cancelText,
@@ -34,112 +55,125 @@ const DatePicker: React.ForwardRefRenderFunction<IDatePickerRef, IPropsDatePicke
   },
   ref
 ) => {
-  const [themes] = useState(getStyles(theme))
-  const [translate] = useState(getTranslate(language))
+    const [themes] = useState(getStyles(theme));
+    const [translate] = useState(getTranslate(language));
 
-  const [isFocused, setIsFocus] = useState<undefined | string>()
-  const [errorMessage, setError] = useState<string>('')
+    const [isFocused, setIsFocus] = useState<undefined | string>();
+    const [errorMessage, setError] = useState<string>("");
 
-  const [valueDate, setValue] = useState<undefined | Date>(undefined)
-  const [openDatePicker, setOpenDatePicker] = useState(false)
+    const [valueDate, setValue] = useState<undefined | Date>(undefined);
+    const [openDatePicker, setOpenDatePicker] = useState(false);
 
-  const handleChangeValue = useCallback((value: Date) => {
-    setValue(value)
-    setOpenDatePicker(false)
-    setIsFocus('')
-    setError('')
-    onSubmitEditing && onSubmitEditing()
-  }, [])
+    const handleChangeValue = useCallback((value: Date) => {
+      setValue(value);
+      setOpenDatePicker(false);
+      setIsFocus("");
+      setError("");
+      onSubmitEditing && onSubmitEditing();
+    }, []);
 
-  const handleOpenModal = useCallback((state: boolean) => {
-    setOpenDatePicker(state)
-    setIsFocus(state ? themes.colors.primary : '')
-  }, [])
+    const handleOpenModal = useCallback((state: boolean) => {
+      setOpenDatePicker(state);
+      setIsFocus(state ? themes.colors.primary : "");
+    }, []);
 
-  const getLocaleDate = useCallback(() => {
-    const defaultReturn = { pattern: 'dd/MM/yyyy', locale: ptBR }
+    const getLocaleDate = useCallback(() => {
+      const defaultReturn = { pattern: "dd/MM/yyyy", locale: ptBR };
 
-    if (language === 'es-py') {
-      defaultReturn.locale = es;
-    }
+      if (language === "es-py") {
+        defaultReturn.locale = es;
+      }
 
-    return defaultReturn
-  }, [])
+      return defaultReturn;
+    }, []);
 
-  useImperativeHandle(ref, () => ({
-    setValueDate: (value) => {
-      if (!(value instanceof Date)) return setError(translate.datePicker.error.dateInvalid)
-      setValue(value)
-    },
-    getValueDate: () => {
-      if (valueDate) return { valueDate: format(valueDate, 'yyyy-MM-dd') }
-    },
-    setErrorDate: (error: string) => setError(error),
-    isValid: () => {
-      return isValidDate(valueDate)
-    },
-    isValidMinAge: (minAge: number) => {
-      if (!valueDate || !isValidDate(valueDate)) return false
+    useImperativeHandle(ref, () => ({
+      setValueDate: (value) => {
+        if (!(value instanceof Date))
+          return setError(translate.datePicker.error.dateInvalid);
+        setValue(value);
+      },
+      getValueDate: () => {
+        if (valueDate) return { valueDate: format(valueDate, "yyyy-MM-dd") };
+      },
+      setErrorDate: (error: string) => setError(error),
+      isValid: () => {
+        return isValidDate(valueDate);
+      },
+      isValidMinAge: (minAge: number) => {
+        if (!valueDate || !isValidDate(valueDate)) return false;
 
-      const yearsAgo = subYears(new Date(), minAge);
-      return isBefore(valueDate, yearsAgo) || isEqual(valueDate, yearsAgo);
-    },
-    clear: () => {
-      setError('')
-      setIsFocus('')
-      setValue(undefined)
-    },
-    focus: () => {
-      setIsFocus(theme?.colors?.primary)
-      setOpenDatePicker(true)
-    },
-  }));
+        const yearsAgo = subYears(new Date(), minAge);
+        return isBefore(valueDate, yearsAgo) || isEqual(valueDate, yearsAgo);
+      },
+      clear: () => {
+        setError("");
+        setIsFocus("");
+        setValue(undefined);
+      },
+      focus: () => {
+        setIsFocus(theme?.colors?.primary);
+        setOpenDatePicker(true);
+      },
+    }));
 
-  return (
-    <Container>
-      <TextLabel
-        focus={isFocused}
-        theme={!!errorMessage.length ? {...themes.font?.label, color: themes.font?.error.color} : themes.font?.label}
-      >
-        {label || translate.datePicker.label}
-      </TextLabel>
-      <ContainerInput
-        onPress={() => handleOpenModal(true)}
-        focus={isFocused}
-        error={!!errorMessage.length ? themes?.font?.error?.color : ""}
-        outline={!!theme?.outline}
-        disable={disabled ? themes?.colors?.disabled : ''}
-      >
-        <RNDatePicker
-          modal
-          mode='date'
-          open={openDatePicker}
-          date={valueDate || new Date()}
-          onConfirm={handleChangeValue}
-          onCancel={() => handleOpenModal(false)}
-          title={title || translate.datePicker.title}
-          cancelText={cancelText || translate.datePicker.cancelText}
-          confirmText={confirmText || translate.datePicker.confirmText}
-        />
-
-        {valueDate
-          ? <TextInput>{format(valueDate, getLocaleDate().pattern, {locale: getLocaleDate().locale})}</TextInput>
-          : <TextPlaceHolder>{placeholder || translate.datePicker.placeholder}</TextPlaceHolder>
-        }
-
-        {showIcon &&
-          <Icon
-            size={25}
-            type="material-community"
-            color={isFocused || "#999"}
-            name={!!isFocused ? "calendar" : "calendar-blank"}
+    return (
+      <Container>
+        <TextLabel
+          focus={isFocused}
+          theme={
+            !!errorMessage.length
+              ? { ...themes.font?.label, color: themes.font?.error.color }
+              : themes.font?.label
+          }
+        >
+          {label || translate.datePicker.label}
+        </TextLabel>
+        <ContainerInput
+          focus={isFocused}
+          disabled={disabled}
+          outline={!!theme?.outline}
+          onPress={() => handleOpenModal(true)}
+          disable={disabled ? themes?.colors?.disabled : ""}
+          error={!!errorMessage.length ? themes?.font?.error?.color : ""}
+        >
+          <RNDatePicker
+            modal
+            mode="date"
+            open={openDatePicker}
+            date={valueDate || new Date()}
+            onConfirm={handleChangeValue}
+            onCancel={() => handleOpenModal(false)}
+            title={title || translate.datePicker.title}
+            cancelText={cancelText || translate.datePicker.cancelText}
+            confirmText={confirmText || translate.datePicker.confirmText}
           />
-        }
-      </ContainerInput>
 
-      <TextError>{errorMessage}</TextError>
-    </Container>
-  );
-};
+          {valueDate ? (
+            <TextInput>
+              {format(valueDate, getLocaleDate().pattern, {
+                locale: getLocaleDate().locale,
+              })}
+            </TextInput>
+          ) : (
+            <TextPlaceHolder>
+              {placeholder || translate.datePicker.placeholder}
+            </TextPlaceHolder>
+          )}
 
-export default forwardRef(DatePicker)
+          {showIcon && (
+            <Icon
+              size={25}
+              type="feather"
+              name="calendar"
+              color={isFocused || "#ccc"}
+            />
+          )}
+        </ContainerInput>
+
+        <TextError theme={themes?.font?.error}>{errorMessage}</TextError>
+      </Container>
+    );
+  };
+
+export default forwardRef(DatePicker);
--- src/inputs/DatePicker/styles.ts
+++ src/inputs/DatePicker/styles.ts
@@ -1,5 +1,5 @@
 // Modules
-import styled, { css } from 'styled-components/native';
+import styled, { css } from "styled-components/native";
 
 // Types
 import {
@@ -7,30 +7,33 @@ import {
   IStyleTextError,
   IStyleTextInput,
   IStyleTextLabel,
-} from '../../Types/DatePicker';
-
+} from "../../Types/DatePicker";
 
 export const Container = styled.View`
-  width: 100%;
   display: flex;
+  flex: 1;
+  width: 100%;
   padding: 8px 0;
 `;
 
 export const TextLabel = styled.Text<IStyleTextLabel>`
   color: #000;
-  margin: 4px ;
+  margin: 4px;
 
-  ${props => (props.theme.color) &&
+  ${(props) =>
+    props.theme.color &&
     css`
       color: ${props.theme.color};
     `}
 
-  ${props => (props.focus) &&
+  ${(props) =>
+    props.focus &&
     css`
       color: ${props.focus};
     `}
 
-  ${props => (props.theme.size) &&
+  ${(props) =>
+    props.theme.size &&
     css`
       font-size: ${props.theme.size}px;
     `}
@@ -39,14 +42,16 @@ export const TextLabel = styled.Text<IStyleTextLabel>`
 export const TextError = styled.Text<IStyleTextError>`
   color: #c80000;
   margin: 2px 8px;
-  flex:1;
+  flex: 1;
 
-  ${props => (props.theme.color) &&
+  ${(props) =>
+    props.theme.color &&
     css`
       color: ${props.theme.color};
     `}
 
-  ${props => (props.theme.size) &&
+  ${(props) =>
+    props.theme.size &&
     css`
       font-size: ${props.theme.size}px;
     `}
@@ -58,33 +63,38 @@ export const ContainerInput = styled.TouchableOpacity<IStyleContainerInput>`
   align-items: center;
   border-color: #ccc;
   border-bottom-width: 1px;
-  padding: 0 8px ;
+  padding: 0 8px;
   height: 40px;
 
-
-  ${props => (props.disable) &&
+  ${(props) =>
+    props.disable &&
     css`
       background-color: ${props.disable};
     `}
 
-  ${props => (props.outline) &&
+  ${(props) =>
+    props.outline &&
     css`
       border: 1px solid #ccc;
       border-radius: 4px;
     `}
 
-  ${props => (props.focus) &&
+  ${(props) =>
+    props.focus &&
     css`
       border-color: ${props.focus};
     `}
 
-  ${props => (props.error) &&
+  ${(props) =>
+    props.error &&
     css`
       border-color: ${props.error};
       border-bottom-width: 1px;
     `}
 
-  ${props => (!!props.error && props.outline) &&
+  ${(props) =>
+    !!props.error &&
+    props.outline &&
     css`
       border-color: ${props.error};
       border-bottom-width: 1px;
@@ -93,16 +103,17 @@ export const ContainerInput = styled.TouchableOpacity<IStyleContainerInput>`
     `}
 `;
 
-
 export const TextInput = styled.Text<IStyleTextInput>`
   flex: 1;
 
-  ${props => (props.theme.color) &&
+  ${(props) =>
+    props.theme.color &&
     css`
       color: ${props.theme.color};
     `}
 
-  ${props => (props.theme.size) &&
+  ${(props) =>
+    props.theme.size &&
     css`
       font-size: ${props.theme.size}px;
     `}
